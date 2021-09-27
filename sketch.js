2c3c40cf5aa23245688b47bc71bfcc1efdbae9a0
ABHINAV2526 <82525258+ABHINAV2526@users.noreply.github.com>
2021-09-27 17:37:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3,box4;
+var hero,monster,rope,ground;
+
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  hero = new Hero(400,800,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
+
+  box1A = new Box(600, 100, 70, 70);
+  box2A= new Box(600, 100, 70, 70);
+  box3A = new Box(600, 100, 70, 70);
+  box4A = new Box(600, 100, 70, 70);
+
+  box1AB = new Box(700, 100, 70, 70);
+  box2AB = new Box(700, 100, 70, 70);
+  box3AB = new Box(700, 100, 70, 70);
+  box4AB = new Box(700, 100, 70, 70);
+  box5AB = new Box(700, 100, 70, 70);
+  box6AB = new Box(700, 100, 70, 70);
+  box7AB = new Box(700, 100, 70, 70);
+  
+  box1ABC = new Box(800, 100, 70, 70);
+  box2ABC = new Box(800, 100, 70, 70);
+  box3ABC = new Box(800, 100, 70, 70);
+
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+  box6 = new Box(900, 100, 70, 70);
+  box7 = new Box(900, 100, 70, 70);
+  
+}
+
+function draw() {
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+
+  box1A.display();
+  box2A.display();
+  box3A.display();
+  box4A.display()
+  
+
+  box1AB.display();
+  box2AB.display();
+  box3AB.display();
+  box4AB.display()
+  box5AB.display()
+  box6AB.display()
+  box7AB.display()
+
+  box1ABC.display();
+  box2ABC.display();
+  box3ABC.display();
+
+ 
+  box2.display();
+  box3.display();
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+
+
+
+  hero.display();
+  rope.display();
+  monster.display();
+
+  
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(hero.body, { x: mouseX, y: mouseY });
+}
